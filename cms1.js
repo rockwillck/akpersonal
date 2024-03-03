21f2adfc776556c9362ba9da4a01396e7c7f56fb
rockwillck <38663064+rockwillck@users.noreply.github.com>
2024-03-02 21:05:21 -0600

    changed to tsv

--- cms1.js
+++ cms1.js
@@ -1,48 +1,51 @@
-const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQQAzTdTPnWJjcU2bHPhsx005aGBU1nJ2okA9beZ68bQD0bte2BfcFZjQxModDjX9NsB-aJoPTEB6H-/pub?gid=0&single=true&output=csv';
+const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQQAzTdTPnWJjcU2bHPhsx005aGBU1nJ2okA9beZ68bQD0bte2BfcFZjQxModDjX9NsB-aJoPTEB6H-/pub?gid=0&single=true&output=tsv';
+
 
 // Function to fetch the CSV file
 const fetchCSV = async () => {
-  try {
-    const response = await fetch(csvUrl);
-    if (!response.ok) {
-      throw new Error('Failed to fetch CSV');
-    }
-    const csvData = await response.text();
-    
-    // Process the CSV data here (e.g., parse it, display it, etc.)
-    process(csvData);
-    // Now you can work with the CSV data as needed
-    
-  } catch (error) {
-    console.error('Error fetching CSV:', error);
-  }
+ try {
+   const response = await fetch(csvUrl);
+   if (!response.ok) {
+     throw new Error('Failed to fetch CSV');
+   }
+   const csvData = await response.text();
+  
+   // Process the CSV data here (e.g., parse it, display it, etc.)
+   process(csvData);
+   // Now you can work with the CSV data as needed
+  
+ } catch (error) {
+   console.error('Error fetching CSV:', error);
+ }
 };
 
+
 function process(csvData) {
-    rowsSplit = csvData.split("\n")
-    first = rowsSplit[0].split(",")
-    let whoami = []
-    let whatido = []
-    let academics = []
-    for (y = 1; y < rowsSplit.length; y++) {
-        if (rowsSplit[y].split(",")[0].trim() != "") {
-            whoami.push(rowsSplit[y].split(",")[0])
-        }
-        if (rowsSplit[y].split(",")[1].trim() != "") {
-            whatido.push(rowsSplit[y].split(",")[1])
-        }
-        if (rowsSplit[y].split(",")[2].trim() != "") {
-            academics.push(rowsSplit[y].split(",")[2])
-        }
-    }
-    populate("whoami", whoami)
-    populate("whatido", whatido)
-    populate("academics", academics)
+   rowsSplit = csvData.split("\n")
+   first = rowsSplit[0].split("\t")
+   let whoami = []
+   let whatido = []
+   let academics = []
+   for (y = 1; y < rowsSplit.length; y++) {
+       if (rowsSplit[y].split("\t")[0].trim() != "") {
+           whoami.push(rowsSplit[y].split("\t")[0])
+       }
+       if (rowsSplit[y].split("\t")[1].trim() != "") {
+           whatido.push(rowsSplit[y].split("\t")[1])
+       }
+       if (rowsSplit[y].split("\t")[2].trim() != "") {
+           academics.push(rowsSplit[y].split("\t")[2])
+       }
+   }
+   populate("whoami", whoami)
+   populate("whatido", whatido)
+   populate("academics", academics)
 }
 fetchCSV()
 
+
 function populate(id, array) {
-    for (string of array) {
-        document.getElementById(id).innerHTML += `<li>${string}</li>`
-    }
+   for (string of array) {
+       document.getElementById(id).innerHTML += `<li>${string}</li>`
+   }
 }
